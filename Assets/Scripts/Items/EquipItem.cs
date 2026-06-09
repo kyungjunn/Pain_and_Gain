a02c27507f6830ed5131be8b17ead946c747cf08
seeun <msethrc@gmail.com>
2026-06-10 07:29:43 +0900

    아이템 상호작용 로그 출력

--- Assets/Scripts/Items/EquipItem.cs
+++ Assets/Scripts/Items/EquipItem.cs
@@ -6,5 +6,13 @@ public class EquipItem : ItemBase
     {
         float bonusAttackDamage = itemData.BonusAttackDamage;
 
+        if (player != null)
+        {
+            Debug.Log($"[장비 적용] {player.name} 에게 데미지 +{bonusAttackDamage} 증가");
+        }
+        else
+        {
+            Debug.Log($"[테스트] 가져온 공격력: {bonusAttackDamage}");
+        }
     }
 }
