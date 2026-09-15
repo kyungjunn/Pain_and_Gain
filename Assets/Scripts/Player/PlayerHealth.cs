f661946d3bad42d2cae64a68102d9d569eef680d
KimKyungJun <1124kyungjun@naver.com>
2026-09-15 18:05:41 +0900

    fix: 증강 선택 후 현재 체력 유지

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using UnityEngine;
 
-// 플레이어 현재 체력, 피격, 레벨업 회복을 관리
+// 플레이어 현재 체력, 피격, 회복을 관리
 public class PlayerHealth : LivingEntity
 {
     [SerializeField] private PlayerStats stats;
@@ -13,8 +13,6 @@ public class PlayerHealth : LivingEntity
     public bool IsDead { get; private set; }
     public Action onHealthChanged;
 
-    private int cachedMaxHealth;
-    private PlayerLevelSystem levelSystem;
     private PlayerStateManager stateManager;
 
     private void Awake()
@@ -24,11 +22,9 @@ public class PlayerHealth : LivingEntity
             stats = GetComponent<PlayerStats>();
         }
 
-        levelSystem = GetComponent<PlayerLevelSystem>();
         stateManager = GetComponent<PlayerStateManager>();
 
-        cachedMaxHealth = MaxHealth;
-        CurrentHealth = cachedMaxHealth;
+        CurrentHealth = MaxHealth;
     }
 
     private void OnEnable()
@@ -37,16 +33,6 @@ public class PlayerHealth : LivingEntity
         {
             stats.onStatsChanged += HandleStatsChanged;
         }
-
-        if (levelSystem == null)
-        {
-            levelSystem = GetComponent<PlayerLevelSystem>();
-        }
-
-        if (levelSystem != null)
-        {
-            levelSystem.onLevelUp += HandleLevelUp;
-        }
     }
 
     private void OnDisable()
@@ -54,11 +40,6 @@ public class PlayerHealth : LivingEntity
         if (stats != null)
         {
             stats.onStatsChanged -= HandleStatsChanged;
-        }
-
-        if (levelSystem != null)
-        {
-            levelSystem.onLevelUp -= HandleLevelUp;
         }
     }
 
@@ -98,24 +79,7 @@ public class PlayerHealth : LivingEntity
     private void HandleStatsChanged()
     {
         int newMaxHealth = MaxHealth;
-        int healthDifference = newMaxHealth - cachedMaxHealth;
-
-        // HP 증강으로 최대 체력이 늘어난 경우 늘어난 만큼 현재 체력도 보정
-        if (healthDifference > 0)
-        {
-            CurrentHealth += healthDifference;
-        }
-
-        cachedMaxHealth = newMaxHealth;
-        CurrentHealth = Mathf.Clamp(CurrentHealth, 0, cachedMaxHealth);
-        onHealthChanged?.Invoke();
-    }
-
-    private void HandleLevelUp()
-    {
-        // 레벨업 보상으로 즉시 최대 체력까지 회복
-        cachedMaxHealth = MaxHealth;
-        CurrentHealth = cachedMaxHealth;
+        CurrentHealth = Mathf.Clamp(CurrentHealth, 0, newMaxHealth);
         onHealthChanged?.Invoke();
     }
 
